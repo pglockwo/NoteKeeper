297c11f17bf3beb5be460e8b25da9fa35c717eb3
lockpa <Paul.Lockwood@pearson.com>
2019-12-05 16:48:49 +0000

    added a .net core package

---
+++ NoteKeeper.Core.Tests/Basic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoteKeeper.ViewModels;
+
+namespace NoteKeeper.Tests
+{
+    [TestClass]
+    public class Basic
+    {
+        [TestMethod]
+        public void can_do_something_basic()
+        {
+            var itemDetailViewModel = new ItemDetailViewModel();
+
+            Assert.IsNotNull(itemDetailViewModel);
+        }
+    }
+}
